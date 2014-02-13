fa3704a4c93b5a72b7d6d34e7a06e7dadedc5d2f
Reena <lthaveet@macalester.edu>
2014-02-12 18:47:54 -0600

    hw 3

--- src/edu/macalester/comp124/GuessNumber.java
+++ src/edu/macalester/comp124/GuessNumber.java
@@ -6,11 +6,48 @@ import java.util.Random;
 
 public class GuessNumber extends ConsoleProgram {
     private final Random rand = new Random();
-    
-    public void run() {
+
+    public void run(){
+
+        while(true){
+            game();
+            String again = readLine("Would you like to play again?(true or false)");
+            if(again.equals("false")){
+             break;
+
+        }
+    }
+    }
+
+
+    private void game(){
         int answer = rand.nextInt(100);
-        
+    println("I'm thinking of a number between 0 and 99");
+
+        while(true){
+
+        int user = readInt("Your guess : ");
+
+        if(user < answer){
+            println("Higher.");
+        }
+        else if(user > answer){
+            println("Lower");
+        }
+        else if(user == answer){
+            break;
+        }
+
+    }
+        println("You Got It!");
         // TODO: Replace this with your code to make the user guess the answer
         println("The answer is " + answer);
     }
 }
+
+
+
+
+
+
+
